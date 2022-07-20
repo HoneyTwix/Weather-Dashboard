c8579c861ddbe0eb7b16ff17eeb04eded1cfed86
HoneyTwix <chocolateenchanted@gmail.com>
2022-07-20 11:09:00 -0500

    weather

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
-// Weather API 
-async function getWeatherInfo(longitude, latitude) {
-    const apiweatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=722979637d7f44411802fb9c1783e210&units=imperial`;
-    const result = await fetch(apiweatherUrl).then(response => response.json())
-    return result
-}
+// Display city location and weather info 
+function displayCityOverview(cityName, weather) {
+    const todayDay = new Date ()
+    const title = document.getElementById("overview-title")
+    title.innerHTML = `${cityName} (${todayDay.toDateString()})`; 
+    const temperatureElement = document.getElementById("Temp")
+    temperatureElement.innerHTML = `Temp: ${weather.current.temp} &degF` 
+    const windElement = document.getElementById("Wind")
+    windElement.innerHTML = `Wind: ${weather.current.wind_speed} MPH`
+    const humidityElement = document.getElementById("Humidity")
+    humidityElement.innerHTML = `Humidity: ${weather.current.humidity} %`
+    const ultraElement = document.getElementById('UV-Index')
+    ultraElement.innerHTML = `UV-Index: ${weather.current.uvi}`
+    }
+    
 
-async function getCityLocation(city) {
-    const apiGeoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=722979637d7f44411802fb9c1783e210`
-    const response = await fetch(apiGeoURL).then(response => response.json())
-    const cityData = response[0] 
-    const result = { longitude: cityData.lon, latitude: cityData.lat, name: cityData.name, state: cityData.state, country: cityData.country }
-    return result
-}
+
+
+
+
+
+
+
+
 // Search Button
 async function handleSearch() {
-    const textInput = document.getElementById("searchInput").value
-    const cityLocation = await getCityLocation(textInput)
-    const weather = await getWeatherInfo(cityLocation.longitude, cityLocation.latitude)
+    const userInput = document.getElementById("searchInput").value
+    const cityLocation = await cityLongandLat(userInput)
+    const weather = await weatherDate(cityLocation.longitude, cityLocation.latitude)
     displayCityOverview(cityLocation.name, weather)
     displayFiveDayForecast(weather)
 }
 
-// Display city location and weather info 
-function displayCityOverview(cityName, weather) {
-const currentDay = new Date ()
-const overviewTitleEl = document.getElementById("overview-title")
-overviewTitleEl.innerHTML = `${cityName} (${currentDay.toDateString()})`; 
-const tempEl = document.getElementById("Temp")
-tempEl.innerHTML = `Temp: ${weather.current.temp} &degF` 
-const windEl = document.getElementById("Wind")
-windEl.innerHTML = `Wind: ${weather.current.wind_speed} MPH`
-const humidityEl = document.getElementById("Humidity")
-humidityEl.innerHTML = `Humidity: ${weather.current.humidity} %`
-const uvEl = document.getElementById('UV-Index')
-uvEl.innerHTML = `UV-Index: ${weather.current.uvi}`
+// City Buttons
+const cityButtons = document.getElementsByClassName('city')
+
+for (var i = 0; i < cityButtons.length; i++){
+    let btn = cityButtons.item(i)
+    btn.addEventListener("click", function(){
+        document.getElementById("searchInput").value = btn.innerHTML
+        handleSearch();
+    
+    })
+}
+
+
+
+async function cityLongandLat(city) {
+    const weatherURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=722979637d7f44411802fb9c1783e210`
+    const response = await fetch(weatherURL).then(response => response.json())
+    const infoCity = response[0] 
+    const data = { longitude: infoCity.lon, latitude: infoCity.lat, name: infoCity.name, state: infoCity.state, country: infoCity.country }
+    return data
 }
 
 // Five Day Forecast 
@@ -43,29 +60,26 @@ const forecastCards = document.getElementById("forecast").children
 for (let i = 0; i < 5; i ++){
 
 const forecast = weather.daily[i + 1]
-const date = new Date(forecast.dt * 1000) // dt is a UNIX timestamp. Multiply it by 1000 so that to make the date from milliseconds, and not seconds
+const date = new Date(forecast.dt * 1000)
 
-const firstCardItems = forecastCards.item(i).children
-firstCardItems.item(0).innerHTML = date.toDateString()
-firstCardItems.item(1).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`
-firstCardItems.item(2).innerHTML = `Temp: ${forecast.temp.day} &degF`
-firstCardItems.item(3).innerHTML = `Wind: ${forecast.wind_speed} MPH`
-firstCardItems.item(4).innerHTML = `Humidity: ${forecast.humidity} %`
+const cards = forecastCards.item(i).children
+cards.item(0).innerHTML = date.toDateString()
+cards.item(1).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`
+cards.item(2).innerHTML = `Temp: ${forecast.temp.day} &degF`
+cards.item(3).innerHTML = `Wind: ${forecast.wind_speed} MPH`
+cards.item(4).innerHTML = `Humidity: ${forecast.humidity} %`
 }}
 
 // Declaring Variables 
-const searchBtn = document.getElementById('searchbtn')
-searchBtn.addEventListener("click", handleSearch)
+const searchbutton = document.getElementById('searchbutton')
+searchbutton.addEventListener("click", handleSearch)
 
-// City Buttons
-const cityBtns = document.getElementsByClassName('city')
 
-for (var i = 0; i < cityBtns.length; i++){
-    let btn = cityBtns.item(i)
-    btn.addEventListener("click", function(){
-        document.getElementById("searchInput").value = btn.innerHTML
-        handleSearch();
-    
-    })
+// Weather API 
+async function weatherDate(longitude, latitude) {
+    const apiweatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=722979637d7f44411802fb9c1783e210&units=imperial`;
+    const data = await fetch(apiweatherUrl).then(response => response.json())
+    return data
 }
-cityBtns.addEventListener("click")
+
+cityButtons.addEventListener("click")
