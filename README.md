5859b5d7317e7a1bd52d63b07c28f7d660520973
HoneyTwix <chocolateenchanted@gmail.com>
2022-07-26 00:11:49 -0500

    js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Weather-dashboard
 
-You can search the weather for any city of your choosing and its upcoming weather forecast.
+You can search the weather for any city of your choosing and its upcoming weather days.
 
 [Website](https://honeytwix.github.io/Weather-Dashboard/)
 ![Screenshot](assets/weather.png)
--- js/script.js
+++ js/script.js
@@ -1,85 +1,64 @@
-// Display city location and weather info 
-function displayCityOverview(cityName, weather) {
-    const todayDay = new Date ()
-    const title = document.getElementById("overview-title")
-    title.innerHTML = `${cityName} (${todayDay.toDateString()})`; 
-    const temperatureElement = document.getElementById("Temp")
-    temperatureElement.innerHTML = `Temp: ${weather.current.temp} &degF` 
-    const windElement = document.getElementById("Wind")
-    windElement.innerHTML = `Wind: ${weather.current.wind_speed} MPH`
-    const humidityElement = document.getElementById("Humidity")
-    humidityElement.innerHTML = `Humidity: ${weather.current.humidity} %`
-    const ultraElement = document.getElementById('UV-Index')
-    ultraElement.innerHTML = `UV-Index: ${weather.current.uvi}`
-    }
-    
+apiKey = 'cb2f88f51eb3c681f6de41b723de6fca'
 
+async function cityLocation(city) {
+const cityUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=cb2f88f51eb3c681f6de41b723de6fca`
+apiResponse = await fetch(cityUrl).then(response=> response.json())
+cityData = apiResponse[0]
+wantedData = {latitude: cityData.lat, longitude: cityData.lon, name: cityData.name, state: cityData.state, country: cityData.country}
+return wantedData
+}
+async function getWeather(latitude,longitude){
+const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=cb2f88f51eb3c681f6de41b723de6fca&units=imperial`
+const result = await fetch(weatherUrl).then(response=> response.json())
+return result}
 
+async function handleSearch(){
+const input = document.getElementById('searchy').value
+const cityLocal = await cityLocation(input)
+const weather = await getWeather(cityLocal.latitude,cityLocal.longitude)
+showCity(cityLocal.name, weather)
+showFivedayWeather(weather)}
 
+function showCity(cityName, weather){
+const currentDay = new Date ()
+const humiditybox = document.getElementById("humidity")
+humiditybox.innerHTML = `The City's Humidity: ${weather.current.humidity} %`    
+const overviewBox = document.getElementById("overview-title")
+overviewBox.innerHTML = `${cityName} (${currentDay.toDateString()})`;
+const uvBox = document.getElementById('uv-index')
+uvBox.innerHTML = `The City's UV-Index: ${weather.current.uvi}` 
+const temperatureBox = document.getElementById("temperature")
+temperatureBox.innerHTML = `The City's Temperature: ${weather.current.temp} &degF` 
+const windBox = document.getElementById("wind")
+windBox.innerHTML = `The City's Wind Speed: ${weather.current.wind_speed} MPH`
 
-
-
-
-
-
-// Search Button
-async function handleSearch() {
-    const userInput = document.getElementById("searchInput").value
-    const cityLocation = await cityLongandLat(userInput)
-    const weather = await weatherDate(cityLocation.longitude, cityLocation.latitude)
-    displayCityOverview(cityLocation.name, weather)
-    displayFiveDayForecast(weather)
 }
 
-// City Buttons
-const cityButtons = document.getElementsByClassName('city')
-
-for (var i = 0; i < cityButtons.length; i++){
-    let btn = cityButtons.item(i)
-    btn.addEventListener("click", function(){
-        document.getElementById("searchInput").value = btn.innerHTML
-        handleSearch();
+function showFivedayWeather(weather){
+const differentDays = document.getElementById("days").children
+for (let i = 0; i < 5; i ++){
+const days = weather.daily[i + 1]
+const date = new Date(days.dt * 1000)
+const uniqueDay = differentDays.item(i).children
+uniqueDay.item(0).innerHTML = date.toDateString()
+uniqueDay.item(1).src = `https://openweathermap.org/img/wn/${days.weather[0].icon}.png`
+uniqueDay.item(2).innerHTML = `Temperature of the Day: ${days.temp.day} &degF`
+uniqueDay.item(3).innerHTML = `Wind Speed of the Day: ${days.wind_speed} MPH`
+uniqueDay.item(4).innerHTML = `Humidity of the Day: ${days.humidity} %`
+}}
     
+const searchBtn = document.getElementById('button')
+
+
+searchBtn.addEventListener("click", handleSearch)
+    
+
+const citiesList = document.getElementsByClassName('citiesL')
+for (var i = 0; i < citiesList.length; i++){
+let Buttons = citiesList.item(i)
+Buttons.addEventListener("click", function(){
+document.getElementById("searchy").value = Buttons.innerHTML
+handleSearch();
     })
 }
-
-
-
-async function cityLongandLat(city) {
-    const weatherURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=722979637d7f44411802fb9c1783e210`
-    const response = await fetch(weatherURL).then(response => response.json())
-    const infoCity = response[0] 
-    const data = { longitude: infoCity.lon, latitude: infoCity.lat, name: infoCity.name, state: infoCity.state, country: infoCity.country }
-    return data
-}
-
-// Five Day Forecast 
-function displayFiveDayForecast(weather){
-const forecastCards = document.getElementById("forecast").children
-
-for (let i = 0; i < 5; i ++){
-
-const forecast = weather.daily[i + 1]
-const date = new Date(forecast.dt * 1000)
-
-const cards = forecastCards.item(i).children
-cards.item(0).innerHTML = date.toDateString()
-cards.item(1).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`
-cards.item(2).innerHTML = `Temp: ${forecast.temp.day} &degF`
-cards.item(3).innerHTML = `Wind: ${forecast.wind_speed} MPH`
-cards.item(4).innerHTML = `Humidity: ${forecast.humidity} %`
-}}
-
-// Declaring Variables 
-const searchbutton = document.getElementById('searchbutton')
-searchbutton.addEventListener("click", handleSearch)
-
-
-// Weather API 
-async function weatherDate(longitude, latitude) {
-    const apiweatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=722979637d7f44411802fb9c1783e210&units=imperial`;
-    const data = await fetch(apiweatherUrl).then(response => response.json())
-    return data
-}
-
-cityButtons.addEventListener("click")
+cityBtns.addEventListener("click")
